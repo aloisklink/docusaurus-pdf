3f73828d6e09c043c4b0521bbb75bd9be13444b1
Alois Klink <alois.klink@gmail.com>
2020-06-16 15:11:31 +0100

    Test build on other PC

---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+export declare function generatePdf(initialDocsUrl: string, filename: string | undefined, puppeteerArgs: Array<string>): Promise<void>;
+export declare function generatePdfFromBuildSources(buildDirPath: string, firstDocPath: string, baseUrl: string, filename: string | undefined, puppeteerArgs: Array<string>): Promise<void>;
